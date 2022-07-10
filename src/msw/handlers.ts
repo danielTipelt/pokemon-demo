518275c5523cb0eb0505189800a003bbc4a468a1
Daniel Tipelt <daniel.tipelt@gmail.com>
2022-07-10 17:00:16 +0200

    Mock pokemon images

--- src/msw/handlers.ts
+++ src/msw/handlers.ts
@@ -1,4 +1,3 @@
-import { Console } from "console";
 import { DefaultBodyType, PathParams, rest } from "msw";
 import { Pokemon } from "src/types/Pokemon";
 import { PaginatedResource } from "../types/PaginatedResouce";
@@ -6,6 +5,7 @@ import { Pokeball } from "../types/Pokeball";
 import { SimplePokemon } from "../types/SimplePokemon";
 import { pokemons } from "./db/pokemons";
 import { simplePokemons } from "./db/simple-pokemons";
+import { Blob } from "buffer";
 
 export const handlers = [
   rest.get("http://localhost:3000/api/pokeballs", async (req, res, ctx) => {
@@ -45,6 +45,20 @@ export const handlers = [
       const { id } = req.params;
       const pokemon = pokemons.find((pokemon) => pokemon.id === Number(id));
       return res(ctx.json(pokemon || pokemons[0]));
+    }
+  ),
+  rest.get<DefaultBodyType, PathParams, Blob>(
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/*.png",
+    async (req, res, ctx) => {
+      const imageBuffer = await ctx
+        .fetch(`${process.env.NEXT_PUBLIC_LOCAL_URL}/bulbasaur.png`)
+        .then((res) => res.arrayBuffer());
+
+      return res(
+        ctx.set("Content-Length", imageBuffer.byteLength.toString()),
+        ctx.set("Content-Type", "image/jpeg"),
+        ctx.body(imageBuffer as any)
+      );
     }
   ),
 ];
