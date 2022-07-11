a0b334cef1b03bc71b50e1917936cb837c74d26a
Daniel Tipelt <daniel.tipelt@gmail.com>
2022-07-11 10:19:01 +0200

    Style homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,24 @@ import { PokebalCssImage } from "../components/pokebal-css-image";
 const HomePage: NextPage = () => {
   return (
     <Layout>
-      <h1>Make your pokéballs full</h1>
-      <p data-testid="description">
-        You can pick pokémons and prepare your own pokéballs in this app.
-        Excited like hell, right?
-      </p>
-      <PokebalCssImage className="w-24 h-24" />
-      <Link href="/pokeballs">
-        <a data-testid="cta-button">Go catch &apos;em!</a>
-      </Link>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row lg:gap-8">
+          <PokebalCssImage className="w-36 h-36" />
+
+          <div>
+            <h1 className="text-5xl font-bold">Make your pokéballs full</h1>
+            <p className="py-6">
+              You can pick pokémons and prepare your own pokéballs in this app.
+              Excited like hell, right?
+            </p>
+            <Link href="/pokeballs">
+              <a className="btn btn-primary" data-testid="cta-button">
+                Go catch &apos;em!
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
